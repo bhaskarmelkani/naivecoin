9a3edbf5311db48ebf11b48e6c392c80fdf9d8a3
bhaskarmelkani <melkani.bhaskar@gmail.com>
2018-02-11 14:39:59 +0100

    Code Refactor

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const initialPeers = process.env.PEERS ? process.env.PEERS.split(',') : [];
 
 const BlockChain = require('./lib/blockChain');
 const Socket = require('./lib/websocket');
+
 const blockChain = new BlockChain();
 
 const webSocket = new Socket(blockChain, initialPeers);
